9bd39703dd2fe64f16da92756ac3d7941b1f9683
Jayachandu Bandlamudi <jay_bandlamudi@in.ibm.com>
2025-10-28 15:33:06 +0530

    Minor fix salesloft MCP server setup.

--- mcpgateway/toolops/mcp-server-setup/toolops_fastmcp_server.py
+++ mcpgateway/toolops/mcp-server-setup/toolops_fastmcp_server.py
@@ -2,9 +2,9 @@ import httpx
 import json
 from fastmcp import FastMCP
 api_url = "https://api.salesloft.com"
-api_spec_path = "./api_specs/Wipro_Salesloft_Get_all_actions.json"
+api_spec_path = "./mcpgateway/toolops/mcp-server-setup/api_specs/Wipro_Salesloft_Get_all_actions_short.json"
 mcp_server_name = "salesloft get all actions"
-client = httpx.AsyncClient(base_url=api_url)
+client = httpx.AsyncClient(base_url=api_url,headers={"Authorization": "Bearer v2_ak_101644_dcdc81eaf282f78ca2a468e09a35a4e9b38e6712709028d80fe28a537ecd0d6d"})
 openapi_spec=json.load(open(api_spec_path,'r'))
 # Create the MCP server
 mcp = FastMCP.from_openapi(openapi_spec=openapi_spec, client=client, name=mcp_server_name)
@@ -14,10 +14,14 @@ if __name__ == "__main__":
 
 
 '''
-nohup python -m mcpgateway.translate      --stdio "python ./toolops_fastmcp_server.py"      --expose-streamable-http      --expose-sse      --port 9003 &
+nohup python -m mcpgateway.translate      --stdio "python ./toolops_fastmcp_server.py"      --expose-streamable-http      --expose-sse      --port 8001 &
 '''
 
 
+
+# npx @modelcontextprotocol/inspector 
+
+# Authorization: Bearer "v2_ak_101644_dcdc81eaf282f78ca2a468e09a35a4e9b38e6712709028d80fe28a537ecd0d6d"
 
 '''
 
