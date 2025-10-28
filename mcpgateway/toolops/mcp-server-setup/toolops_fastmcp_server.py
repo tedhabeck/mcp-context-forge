dd2305ef02eb04d6d213b67c711b6eef7f7fd922
Jayachandu Bandlamudi <jay_bandlamudi@in.ibm.com>
2025-10-28 20:20:31 +0530

    Removed bearer token from git push.

--- mcpgateway/toolops/mcp-server-setup/toolops_fastmcp_server.py
+++ mcpgateway/toolops/mcp-server-setup/toolops_fastmcp_server.py
@@ -1,16 +1,22 @@
 import httpx
 import json
+import os
 from fastmcp import FastMCP
 api_url = "https://api.salesloft.com"
 api_spec_path = "./mcpgateway/toolops/mcp-server-setup/api_specs/Wipro_Salesloft_Get_all_actions_short.json"
-mcp_server_name = "salesloft get all actions."
-client = httpx.AsyncClient(base_url=api_url,headers={"Authorization": "Bearer v2_ak_101644_dcdc81eaf282f78ca2a468e09a35a4e9b38e6712709028d80fe28a537ecd0d6d"})
+mcp_server_name = "salesloft get all actions"
+SALESLOFT_BEARER_TOKEN = os.environ.get("SALESLOFT_BEARER_TOKEN","")
+client = httpx.AsyncClient(base_url=api_url,headers={"Authorization": "Bearer "+SALESLOFT_BEARER_TOKEN})
 openapi_spec=json.load(open(api_spec_path,'r'))
 # Create the MCP server
 mcp = FastMCP.from_openapi(openapi_spec=openapi_spec, client=client, name=mcp_server_name)
 
 if __name__ == "__main__":
-    mcp.run()
+    if SALESLOFT_BEARER_TOKEN == "":
+        print("Please set SALESLOFT_BEARER_TOKEN asn env variable")
+    else:
+        print("SALESLOFT_BEARER_TOKEN",SALESLOFT_BEARER_TOKEN)
+        mcp.run()
 
 
 '''
@@ -21,48 +27,5 @@ nohup python -m mcpgateway.translate      --stdio "python ./toolops_fastmcp_serv
 
 # npx @modelcontextprotocol/inspector 
 
-# Authorization: Bearer "v2_ak_101644_dcdc81eaf282f78ca2a468e09a35a4e9b38e6712709028d80fe28a537ecd0d6d"
-
-'''
-
-[
-    {
-        "name": "salesloft all actions",
-        "displayName": "salesloft actions",
-        "url": "https://api.salesloft.com/v2/actions",
-        "integration_type": "REST",
-        "request_type": "GET",
-        "description": "Get all actions in salesloft",
-        "auth_type": "bearer",
-        "auth_value": "v2_ak_101644_dcdc81eaf282f78ca2a468e09a35a4e9b38e6712709028d80fe28a537ecd0d6d",
-        "headers": {
-            "Accept": "application/json",
-            "User-Agent": "MCP-Gateway/1.0"
-        },
-        "input_schema": {
-            "type": "object",
-            "properties": {
-                "ids": {
-                    "type": "string",
-                    "description": "the ids of actions"
-                },
-                "step_id": {
-                    "type": "string",
-                    "description": "The ID of the step to retrieve. If not provided, all steps for the pipeline will be returned."
-                },
-                "type": {
-                    "type": "string",
-                    "description": "Filter actions by type. email, phone, integration etc."
-                }
-            },
-            "required": ["ids"]
-        },
-        "jsonpath_filter": "$.main",
-        "tags": [
-            "salesloft","api"
-        ]
-    }
-]
 
 
-'''
