71249e21b3e0a59c591490a7dffa6f5bfebb7f5b
Jayachandu Bandlamudi <jay_bandlamudi@in.ibm.com>
2025-11-24 18:46:32 +0530

    Updated LLM inference to chat models to fix the issue with LLM providers.

--- mcpgateway/toolops/testing/mcp-server-setup/mcp_server_setup.sh
+++ mcpgateway/toolops/testing/mcp-server-setup/mcp_server_setup.sh
@@ -1,3 +1,3 @@
 # Script to start MCP server using OAPI speficifciation , this server is used for toolops testing purposes.
 echo "setting up MCP server for salesloft get all actions OAPI at port 9009"
-python -m mcpgateway.translate      --stdio "python ./mcpgateway/toolops/testing/mcp-server-setup/toolops_fastmcp_server.py"     --expose-sse      --port 9009
+python -m mcpgateway.translate      --stdio "python ./mcpgateway/toolops/testing/mcp-server-setup/toolops_fastmcp_server.py"   --expose-streamable-http   --expose-sse      --port 9009
