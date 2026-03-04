af436f492b2aae43f57cb03e6bf574e5f8861b42
popagruia <popa.gruia@gmail.com>
2026-03-04 03:00:32 +0200

    [FEATURE][PLUGINS]: Add HTTP header filter plugin for sensitive data protection (#3225)
    * New filter plugin for http headers
    
    Signed-off-by: popagruia <adrian.popa@ro.ibm.com>
    
    * Fix lint issues
    
    Signed-off-by: popagruia <adrian.popa@ro.ibm.com>
    
    * Fix pre commit issues
    
    Signed-off-by: popagruia <adrian.popa@ro.ibm.com>
    
    * fix(plugins): fix frozen payload mutation, broken hooks, and field errors in header filter plugin
    
    - Fix frozen PluginPayload mutation: use model_copy(update=...) instead of
      direct attribute assignment which raises ValidationError
    - Remove resource_pre_fetch hook: ResourcePreFetchPayload has no headers field
    - Remove prompt_prehook hook: PromptPrehookPayload has no headers or name field
    - Fix agent_pre_invoke to use payload.agent_id instead of payload.name
    - Use HttpHeaderPayload(root=...) keyword arg matching vault plugin convention
    - Replace Set[str] import with builtin set[str] for Python 3.11+
    - Add test __init__.py for consistency with other plugin test directories
    - Fix README defaults to include X-Vault-Tokens (matches config.yaml)
    - Rewrite tests: 25 tests (up from 14), 100% coverage, all passing
    - Pylint 10.00/10, black/ruff/flake8 clean
    
    Signed-off-by: Mihai Criveti <crivetimihai@gmail.com>
    
    ---------
    
    Signed-off-by: popagruia <adrian.popa@ro.ibm.com>
    Signed-off-by: Mihai Criveti <crivetimihai@gmail.com>
    Co-authored-by: popagruia <adrian.popa@ro.ibm.com>
    Co-authored-by: Mihai Criveti <crivetimihai@gmail.com>

---
+++ plugins/header_filter/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""Header Filter Plugin.
+
+Location: ./plugins/header_filter/__init__.py
+Copyright 2025
+SPDX-License-Identifier: Apache-2.0
+
+Header filter plugin implementation for filtering sensitive headers.
+"""
